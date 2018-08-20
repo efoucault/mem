58e14d86f7774d4d3285b3017085eb7aa892f7c8
Eric Foucault <efoucault@gmail.com>
2018-08-20 19:47:21 +0200

    added model message

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :bookings, foreign_key: "customer_id", dependent: :destroy
   has_many :reviews, foreign_key: "owner_id", dependent: :destroy
   has_many :reviews, foreign_key: "customer_id", dependent: :destroy
+  has_many :messages, foreign_key: "owner_id", dependent: :destroy
+  has_many :messages, foreign_key: "customer_id", dependent: :destroy
     # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
