e7cb393570aa15b6e675d2ecf8b1fef8ab482f20
Ophelie Battaglia <ophelie.battaglia@gmail.com>
2018-08-20 17:01:47 +0200

    creation du model Watch, de ses routes, de son controller, de sa page index, +css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_124153) do
+ActiveRecord::Schema.define(version: 2018_08_20_141642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,22 @@ ActiveRecord::Schema.define(version: 2018_08_20_124153) do
     t.boolean "super_user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "watches", force: :cascade do |t|
+    t.string "photo"
+    t.date "production_year"
+    t.string "brand"
+    t.string "model"
+    t.integer "price_per_day"
+    t.string "gender"
+    t.text "description"
+    t.string "color"
+    t.string "mechanism"
+    t.string "case_material"
+    t.string "style"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
