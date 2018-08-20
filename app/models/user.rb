584f7906add401af17e16ff15e2ba6d767e7c8d0
Eric Foucault <efoucault@gmail.com>
2018-08-20 19:42:39 +0200

    added review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :watches
   has_many :bookings, foreign_key: "owner_id", dependent: :destroy
   has_many :bookings, foreign_key: "customer_id", dependent: :destroy
-  # Include default devise modules. Others available are:
+  has_many :reviews, foreign_key: "owner_id", dependent: :destroy
+  has_many :reviews, foreign_key: "customer_id", dependent: :destroy
+    # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,4 +1,5 @@
 class Watch < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :reviews, through: :bookings
 end
