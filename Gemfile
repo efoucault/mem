f74749966dbccac8c7913f3e817932a9ced11467
Eric Foucault <efoucault@gmail.com>
2018-08-20 16:54:19 +0200

    added avatar on user sign-up and user index

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'uglifier'
 gem 'webpacker'
 
 gem 'devise'
+gem 'cloudinary'
+gem 'carrierwave', '~> 1.2'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_124153) do
+ActiveRecord::Schema.define(version: 2018_08_20_141731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_124153) do
     t.string "address"
     t.text "description"
     t.boolean "super_user"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
