b518d3eb4364e10b3059988f7145d8bd29f5c970
Ophelie Battaglia <ophelie.battaglia@gmail.com>
2018-08-21 14:18:28 +0200

    first steps to create a new watch

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,5 +1,6 @@
 class WatchesController < ApplicationController
 
+
   def index
     @watches = Watch.all
   end
@@ -10,6 +11,25 @@ class WatchesController < ApplicationController
 
   def new
     @watch = Watch.new
+    @collection_brand = ['Chopard', 'Bulgari','IWC', 'Audemars Piguet',  "Bell & Ross"]
+    @collection_gender = ['Homme', 'Femme','Unisex']
+    @collection_color = ['Or', 'Argent', 'Bronze', 'Blanc', 'Noir', 'Marron', 'Bleu', 'Vert', 'Rouge', 'Orange', 'Jaune', 'Beige', 'Gris']
+    @collection_mechanism = ['Automatique (Mécanique)','Energie Solaire','Kinetic','Quartz (Pile)']
+    @collection_case_material = ['Autre','Cuir','Métal','Plastique']
+    @collection_style = ["Vintage", "Sport", "Cérémonie", "Bohème", "Classique", "Design"]
+  end
+
+  def create
+    @watch = Watch.new(watch_params)
+    @watch.user = current_user
+    @watch.save!
+    redirect_to watch_path(@watch)
+  end
+
+  private
+
+  def watch_params
+    params.require(:watch).permit(:photo, :production_year, :brand, :model, :price_per_day, :gender, :description, :color, :mechanism, :case_material, :style)
   end
 
 end
