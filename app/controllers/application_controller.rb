d2856c2c19ded0e0375fb4a55a33523e2b120a25
Eric Foucault <efoucault@gmail.com>
2018-08-21 11:57:32 +0200

    uncomment authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
   #before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
