3c36e28295689cc3922a5309f90aa381a6a296e0
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-08-21 18:44:56 +0200

    adding geocoder

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'webpacker'
 gem 'devise'
 gem 'cloudinary'
 gem 'carrierwave', '~> 1.2'
+gem 'geocoder'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :watches
   has_many :bookings, foreign_key: "owner_id", dependent: :destroy
   has_many :bookings, foreign_key: "customer_id", dependent: :destroy
