27ea8077b2809a242d2199df545f4d38a46f42e3
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-08-22 01:12:58 +0200

    adding map on homepage

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -4,6 +4,15 @@ class WatchesController < ApplicationController
 
   def index
     @watches = Watch.all
+    @users = User.where.not(latitude: nil, longitude: nil)
+
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
   end
 
   def show
