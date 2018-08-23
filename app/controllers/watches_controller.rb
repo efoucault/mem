120479a62e9933248a1f5dd78adfe7745ae08eb0
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-08-23 23:38:36 +0200

    show geolocalisation of the watch inside watch show

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -30,6 +30,24 @@ class WatchesController < ApplicationController
 
   def show
     @watch = Watch.find(params[:id])
+    @users = User.where.not(latitude: nil, longitude: nil)
+
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        # url: link_to @users(user)
+        # label: {
+        #   text: "Watch",
+        #   color: "black",
+        #   fontWeight: "bold",
+        #   fontSize: "16px"g
+        # },
+        icon: "https://res.cloudinary.com/de06zwkul/image/upload/v1535028449/mem-markr.png",
+        infoWindow: { content: render_to_string(partial: 'watches/infobox', locals: {user:user}) }
+
+      }
+    end
   end
 
   def new
