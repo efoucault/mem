2f379b06e23f7b7705eb40705747328bf5e2df75
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-08-25 15:51:56 +0200

    show one user on map and infobox on hover

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -5,7 +5,22 @@ const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
+  // map.addMarkers(markers);
+  // if (markers.length === 0) {
+  //   map.setZoom(2);
+  // } else if (markers.length === 1) {
+  //   map.setCenter(markers[0].lat, markers[0].lng);
+  //   map.setZoom(14);
+  // } else {
+  //   map.fitLatLngBounds(markers);
+  // }
+  const mapMarkers = [];
+  markers.forEach((marker) => {
+    const mapMarker = map.createMarker(marker);
+    mapMarkers.push(mapMarker);
+    map.addMarker(mapMarker);
+  });
+
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
@@ -14,6 +29,17 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   } else {
     map.fitLatLngBounds(markers);
   }
+
+  // Map marker animation
+  // Select all cards
+  const cards = document.querySelectorAll('.card-link');
+  cards.forEach((card, index) => {
+    // Put a microphone on each card listenning for a hover event
+    card.addEventListener('mouseenter', () => {
+      // Here we trigger the display of the corresponding marker infoWindow as it is the default behavior of a click on a  marker
+      google.maps.event.trigger(mapMarkers[index], 'click');
+    });
+  });
 const styles = [{
         "featureType": "all",
         "stylers": [
